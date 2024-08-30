bd00b4a2e8c9dca7aa742ea0cf971c37d8f032fc
June Vic Cadayona <juneviccadayona@CODY-MBP-105s-MacBook-Pro.local>
2024-08-31 04:39:38 +0800

    Add map details

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Livewire\Destinations;
-use App\Livewire\EstablishmentInformation;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/login', function () {
@@ -16,18 +14,15 @@ Route::post('/logout', function () {
     return redirect()->route('login');
 })->name('logout');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/', function () {
-        return view('pages.home');
-    })->name('home');
 
-    Route::get('/destinations', Destinations::class)->name('destinations');
+include 'guests.php';
+
+Route::middleware('auth')->group(function () {
 
     Route::get('/help', function () {
         return view('pages.help');
     })->name('help');
 
-    Route::get('/establishments/{establishment}', EstablishmentInformation::class)->name('establishment.show');
 
     Route::get('/dashboard', function () {
         return view('pages.dashboard');
