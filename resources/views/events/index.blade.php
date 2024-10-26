e5e12d738816321b38536616a889e210953b6d00
June Vic Cadayona <juneviccadayona@CODY-MBP-105s-MacBook-Pro.local>
2024-10-26 20:13:47 +0800

    Update icons

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <div class="d-flex justify-content-end">
         <a href="{{ route('events.create') }}" class="btn btn-primary rounded my-2 py-2 d-flex gap-2">
-            <i class="bx bx-store"></i>
+            <i class="bx bx-calendar"></i>
             Add
         </a>
     </div>
--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <div class="d-flex justify-content-end">
         <a href="{{ route('news.create') }}" class="btn btn-primary rounded my-2 py-2 d-flex gap-2">
-            <i class="bx bx-store"></i>
+            <i class="bx bx-news"></i>
             Add
         </a>
     </div>
